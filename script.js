6a183ce684a5d6d5d28d68bcdb861926ab4b4b83
lu121416kk <146039249+lu121416kk@users.noreply.github.com>
2024-05-02 07:44:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+let musicas = 
+[
+    { titulo: 'Stardew Valley Overture', src: 'musicas/musica1.mp3' },
+    { titulo: 'Cloud Country', src: 'musicas/musica2.mp3' },
+    { titulo: 'Grandpa\'s Theme', src: 'musicas/musica3.mp3' },
+    { titulo: 'Settling In', src: 'musicas/musica4.mp3' },
+    { titulo: 'Spring (It\'s A Big World Outside)', src: 'musicas/musica5.mp3' },
+    { titulo: 'Spring (The Valley Comes Alive)', src: 'musicas/musica6.mp3' },
+    { titulo: 'Spring (Wild Horseradish Jam)', src: 'musicas/musica7.mp3' },
+    { titulo: 'Pelican Town', src: 'musicas/musica8.mp3' },
+    { titulo: 'Flower Dance', src: 'musicas/musica9.mp3' },
+    { titulo: 'Fun Festival', src: 'musicas/musica10.mp3' },
+    { titulo: 'Distant Banjo', src: 'musicas/musica11.mp3' },
+    { titulo: 'A Glimpse Of The Other World (Wizard\'s Theme)', src: 'musicas/musica12.mp3' },
+    { titulo: 'Summer (Nature\'s Crescendo)', src: 'musicas/musica13.mp3' },
+    { titulo: 'Summer (The Sun Can Bend An Orange Sky)', src: 'musicas/musica14.mp3' },
+    { titulo: 'Summer (Tropicala)', src: 'musicas/musica15.mp3' },
+    { titulo: 'The Adventure Guild', src: 'musicas/musica16.mp3' },
+    { titulo: 'The Stardrop Saloon', src: 'musicas/musica17.mp3' },
+    { titulo: 'Luau Festival', src: 'musicas/musica18.mp3' },
+    { titulo: 'Dance Of The Moonlight Jellies', src: 'musicas/musica19.mp3' },
+    { titulo: 'Fall (The Smell Of Mushroom)', src: 'musicas/musica20.mp3' },
+    { titulo: 'Fall (Ghost Synth)', src: 'musicas/musica21.mp3' },
+    { titulo: 'Fall (Raven\'s Descent)', src: 'musicas/musica22.mp3' },
+    { titulo: 'The Library And Museum', src: 'musicas/musica23.mp3' },
+    { titulo: 'Stardew Valley Fair Theme', src: 'musicas/musica24.mp3' },
+    { titulo: 'Festival Game', src: 'musicas/musica25.mp3' },
+    { titulo: 'Spirit\'s Eve Festival', src: 'musicas/musica26.mp3' },
+    { titulo: 'Winter (Nocturne Of Ice)', src: 'musicas/musica27.mp3' },
+    { titulo: 'Winter (The Wind Can Be Still)', src: 'musicas/musica28.mp3' },
+    { titulo: 'Winter (Ancient)', src: 'musicas/musica29.mp3' },
+    { titulo: 'Winter Festival', src: 'musicas/musica30.mp3' },
+    { titulo: 'A Golden Star Is Born', src: 'musicas/musica31.mp3' },
+    { titulo: 'Country Shop', src: 'musicas/musica32.mp3' },
+    { titulo: 'Night Market', src: 'musicas/musica33.mp3' },
+    { titulo: 'Submarine Theme', src: 'musicas/musica34.mp3' },
+    { titulo: 'Mermaid Song', src: 'musicas/musica35.mp3' },
+    { titulo: 'Calico Desert', src: 'musicas/musica36.mp3' },
+    { titulo: 'Playful', src: 'musicas/musica37.mp3' },
+    { titulo: 'Buttercup Melody', src: 'musicas/musica38.mp3' },
+    { titulo: 'Pleasant Memory (Penny\'s Theme)', src: 'musicas/musica39.mp3' },
+    { titulo: 'Piano Solo (Elliott\'s Theme)', src: 'musicas/musica40.mp3' },
+    { titulo: 'Land Of Green and Gold (Leah\'s Theme)', src: 'musicas/musica41.mp3' },
+    { titulo: 'A Stillness In The Rain (Abigail\'s Melody)', src: 'musicas/musica42.mp3' },
+    { titulo: 'Starwatcher (Maru\'s Theme)', src: 'musicas/musica43.mp3' },
+    { titulo: 'A Sad Song (Alex\'s Theme)', src: 'musicas/musica44.mp3' },
+    { titulo: 'Pickle Jar Rag (Haley\'s Theme)', src: 'musicas/musica45.mp3' },
+    { titulo: 'Echos (Sebastian\'s Theme)', src: 'musicas/musica46.mp3' },
+    { titulo: 'Grapefruit Sky (Dr. Harvey\'s Theme)', src: 'musicas/musica47.mp3' },
+    { titulo: 'Frozen Pizza And Eggs (Shane\'s Theme)', src: 'musicas/musica48.mp3' },
+    { titulo: 'Song Of Feathers (Emily\'s Theme)', src: 'musicas/musica49.mp3' },
+    { titulo: 'Dreamscape', src: 'musicas/musica50.mp3' },
+    { titulo: 'Emily\'s Dance', src: 'musicas/musica51.mp3' },
+    { titulo: 'Alex\'s Keepsake', src: 'musicas/musica52.mp3' },
+    { titulo: 'Band Practice', src: 'musicas/musica53.mp3' },
+    { titulo: 'Sam\'s Band (Electronic Version)', src: 'musicas/musica54.mp3' },
+    { titulo: 'Sam\'s Band (Pop Version)', src: 'musicas/musica55.mp3' },
+    { titulo: 'Sam\'s Band (Bluegrass Version)', src: 'musicas/musica56.mp3' },
+    { titulo: 'Sam\'s Band (Heavy Version)', src: 'musicas/musica57.mp3' },
+    { titulo: 'A Dark Corner Of The Past', src: 'musicas/musica58.mp3' },
+    { titulo: 'Music Box Song', src: 'musicas/musica59.mp3' },
+    { titulo: 'Jaunty', src: 'musicas/musica60.mp3' },
+    { titulo: 'Violin Solo', src: 'musicas/musica61.mp3' },
+    { titulo: 'Wedding Celebration', src: 'musicas/musica62.mp3' },
+    { titulo: 'Mines (Crystal Bells)', src: 'musicas/musica63.mp3' },
+    { titulo: 'Mines (A Flicker In The Deep)', src: 'musicas/musica64.mp3' },
+    { titulo: 'Mines (Star Lumpy)', src: 'musicas/musica65.mp3' },
+    { titulo: 'Mines (Icicles)', src: 'musicas/musica66.mp3' },
+    { titulo: 'Mines (Marimba Of Frozen Bones)', src: 'musicas/musica67.mp3' },
+    { titulo: 'Mines (Cloth)', src: 'musicas/musica68.mp3' },
+    { titulo: 'Mines (Visitor To The Unknown)', src: 'musicas/musica69.mp3' },
+    { titulo: 'Mines (The Lava Dwellers)', src: 'musicas/musica70.mp3' },
+    { titulo: 'Mines (Magical Shoes)', src: 'musicas/musica71.mp3' },
+    { titulo: 'Mines (Danger!)', src: 'musicas/musica72.mp3' },
+    { titulo: 'In The Deep Woods', src: 'musicas/musica73.mp3' },
+    { titulo: 'Journey Of The Prairie King (Overworld)', src: 'musicas/musica74.mp3' },
+    { titulo: 'Journey Of The Prairie King (The Outlaw)', src: 'musicas/musica75.mp3' },
+    { titulo: 'Journey Of The Prairie King (Final Boss & Ending)', src: 'musicas/musica76.mp3' },
+    { titulo: 'Load Game', src: 'musicas/musica77.mp3' },
+    { titulo: 'Sun Room (Alone With Relaxing Tea)', src: 'musicas/musica78.mp3' },
+    { titulo: 'Grapefruit Sky (Pasta Primavera Mix)', src: 'musicas/musica79.mp3' },
+    { titulo: 'The Happy Junimo Show Theme', src: 'musicas/musica80.mp3' },
+    { titulo: 'Movie Theater', src: 'musicas/musica81.mp3' },
+    { titulo: 'Crane Game', src: 'musicas/musica82.mp3' },
+    { titulo: 'Wumbus (Movie Theme)', src: 'musicas/musica83.mp3' },
+    { titulo: 'Exploring Our Vibrant World (Movie Theme)', src: 'musicas/musica84.mp3' },
+    { titulo: 'The Zuzu City Express (Movie Theme)', src: 'musicas/musica85.mp3' },
+    { titulo: 'Movie Theater (Closing Time)', src: 'musicas/musica86.mp3' },
+    { titulo: 'JunimoKart (Title Theme)', src: 'musicas/musica87.mp3' },
+    { titulo: 'JunimoKart (The Gem Sea Giant)', src: 'musicas/musica88.mp3' },
+    { titulo: 'JunimoKart (Slomp\'s Stomp)', src: 'musicas/musica89.mp3' },
+    { titulo: 'JunimoKart (Ghastly Galleon)', src: 'musicas/musica90.mp3' },
+    { titulo: 'JunimoKart (Glowshroom Grotto)', src: 'musicas/musica91.mp3' },
+    { titulo: 'Ginger Island', src: 'musicas/musica92.mp3' },
+    { titulo: 'Professor Snail\'s Radio', src: 'musicas/musica93.mp3' },
+    { titulo: 'Volcano Mines (Molten Jelly)', src: 'musicas/musica94.mp3' },
+    { titulo: 'Volcano Mines (Forgotten World)', src: 'musicas/musica95.mp3' },
+    { titulo: 'Mystery Of The Caldera', src: 'musicas/musica96.mp3' },
+    { titulo: 'The Gourmand\'s Cave', src: 'musicas/musica97.mp3' },
+    { titulo: 'Pirate Theme', src: 'musicas/musica98.mp3' },
+    { titulo: 'Leo\'s Song', src: 'musicas/musica99.mp3' },
+    { titulo: 'Summit Celebration', src: 'musicas/musica100.mp3' }
+]
+
+
+let musica = document.querySelector('.som audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let nomeMusica = document.querySelector('.descricao h2');
+
+duracaoMusica.textContent = segundosMinutos(Math.floor(musica.duration));
+
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+document.querySelector('.botao-pause').addEventListener('click', pararMusica);
+document.querySelector('.anterior').addEventListener('click', () =>{
+    indexMusica--;
+    if(indexMusica < 0){
+        indexMusica = 99;
+    }
+    rendereizarMusica(indexMusica);
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'inline-blocks';
+})
+document.querySelector('.proximo').addEventListener('click', () =>{
+    indexMusica++;
+    if(indexMusica > 99){
+        indexMusica = 0;
+    }
+    rendereizarMusica(indexMusica);
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'inline-block';
+})
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+function rendereizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        duracaoMusica.textContent = segundosMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica() {
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'inline-block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pararMusica() {
+    musica.pause();
+    document.querySelector('.botao-play').style.display = 'inline-block';
+    document.querySelector('.botao-pause').style.display = 'none';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'; 
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosMinutos(segundo){
+    let campoMinuto = Math.floor(segundo / 60);
+    let campoSegundo = segundo % 60;
+    if(campoSegundo < 10 ){
+        campoSegundo = '0' + campoSegundo;
+    }
+
+    return campoMinuto+':'+campoSegundo;
+}
+
+var x = document.getElementById("music"); 
+
+function mouseOver() { 
+    x.play(); 
+} 
+
+var y = document.getElementById("music2"); 
+
+function mouseOvery() { 
+    y.play(); 
+} 
+
+document.querySelector('.totem').addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+})
+
